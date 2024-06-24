de29b9a1f7a3080f967395104f83cfa46b4fd3a2
ashmit1795 <ashmitpatra77@gmail.com>
2024-06-24 14:30:32 +0530

    Display "delivered" on the orders page, if today's date is past the delivery date.

--- scripts/orders.js
+++ scripts/orders.js
@@ -63,7 +63,17 @@ function renderOrderDetailsGrid(order){
     let orderDetailsGridHTML = ``;
     
     order.products.forEach((product) =>{
-        const estimatedDeliveryTimeString = dayjs(product.estimatedDeliveryTime).format('MMMM D');
+        const today = dayjs();
+        const estimatedDeliveryTime = dayjs(product.estimatedDeliveryTime);
+        const estimatedDeliveryTimeString = estimatedDeliveryTime.format('MMMM D');
+        const isDelivered = today.isAfter(estimatedDeliveryTime) || today.isSame(estimatedDeliveryTime);
+
+        let deliveredHTML = '<p class = "delivered"><svg class="check" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="green" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>Delivered</p>';
+
+        let notDeliveredHTML = '<p class = "not-delivered"><svg class="cross" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="red" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>Not Delivered</p>';
+        
+        let isDeliveredHTML = isDelivered ? deliveredHTML : notDeliveredHTML ;
+
         orderDetailsGridHTML += `
             <div class="product-image-container">
                 <img src= ${getProduct(product.productId).image}>
@@ -74,7 +84,8 @@ function renderOrderDetailsGrid(order){
                     ${getProduct(product.productId).name}
                 </div>
                 <div class="product-delivery-date">
-                    Arriving on: ${estimatedDeliveryTimeString}
+                    ${isDelivered ? 'Delivered On': 'Arriving On'}: ${estimatedDeliveryTimeString}
+
                 </div>
                 <div class="product-quantity">
                     Quantity: ${product.quantity}
@@ -91,7 +102,10 @@ function renderOrderDetailsGrid(order){
                         Track package
                     </button>
                 </a>
+                ${isDeliveredHTML}
             </div>
+
+            
         `
     });
 
