5cea973f6d1f7dee050a2a60eccb4e99e5cbd772
ashmit1795 <139261819+ashmit1795@users.noreply.github.com>
2024-06-26 14:16:33 +0530

    Update checkoutHeader.js

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -3,7 +3,7 @@ export function renderCheckoutHeader(qty){
     let checkoutHeaderHTML = 
     `
         Checkout (<a class="return-to-home-link"
-            href="amazon.html"><span class="cart-quantity">${qty}</span> items</a>)
+            href="index.html"><span class="cart-quantity">${qty}</span> items</a>)
     `;
 
     document.querySelector(".checkout-header-middle-section").innerHTML = checkoutHeaderHTML;
